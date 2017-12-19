fb095c49a34df414bf8c6112b41def5f54d244fa
Peter Mitrano <mitranopeter@gmail.com>
2017-12-18 21:03:01 -0500

    implmented circles
     - using Konva
     - drag works
     - ctrl click anywhere adds
     - shift click deletes

--- custom.js
+++ custom.js
@@ -12,52 +12,13 @@ let stage = new Konva.Stage({
   container: 'canvas',
   width: width,
   height: height,
+  fill: '#ff0',
 });
 
 let layer = new Konva.Layer();
 let radius = stage.getWidth() / 100;
 let stroke = 2;
 let line_height = 2;
-
-function add_marker(x_pos, y_pos, layer) {
-  let marker = new Konva.Circle({
-    x: x_pos,
-    y: y_pos,
-    radius: radius,
-    fill: '#00000055',
-    stroke: 'black',
-    strokeWidth: stroke,
-    draggable: true,
-    dragBoundFunc: function(pos) {
-      return {
-        x: Math.max(radius + stroke,
-            Math.min(stage.getWidth() - radius - stroke, pos.x)),
-        y: this.getAbsolutePosition().y,
-      };
-    },
-  });
-
-  marker.on('mouseover', function() {
-    document.body.style.cursor = 'pointer';
-  });
-  marker.on('mouseout', function() {
-    document.body.style.cursor = 'default';
-  });
-
-  layer.add(marker);
-}
-
-stage.on('contentClick', function(event) {
-  if (event.evt.ctrlKey) {
-    // insert new marker
-    add_marker(event.evt.x, event.evt.y, layer);
-  } else if (event.evt.shiftKey) {
-    // delete marker
-  } else {
-    console.log(layer.shapes);
-    // do nothing on normal click
-  }
-});
 
 let line = new Konva.Rect({
   x: 0,
@@ -66,6 +27,67 @@ let line = new Konva.Rect({
   height: line_height,
   fill: '#666',
 });
-
 layer.add(line);
+
+let background = new Konva.Rect({
+  x: 0,
+  y: 0,
+  width: stage.getWidth(),
+  height: stage.getHeight(),
+  fill: '#fafafa',
+});
+layer.add(background);
+
+let marker = new Konva.Circle({
+  x: stage.getWidth() / 2,
+  y: stage.getHeight() / 2,
+  radius: radius,
+  fill: '#00000055',
+  stroke: 'black',
+  strokeWidth: stroke,
+  draggable: true,
+  dragBoundFunc: function(pos) {
+    return {
+      x: Math.max(radius + stroke,
+          Math.min(stage.getWidth() - radius - stroke, pos.x)),
+      y: this.getAbsolutePosition().y,
+    };
+  },
+});
+
+marker.on('mouseover', function() {
+  document.body.style.cursor = 'pointer';
+});
+marker.on('mouseout', function() {
+  document.body.style.cursor = 'default';
+});
+marker.on('click', function(event) {
+  if (event.evt.shiftKey) {
+    this.destroy();
+    layer.draw();
+  }
+});
+
+layer.on('click', function(event) {
+  if (event.evt.ctrlKey) {
+    // insert new marker
+    add_marker(stage.getPointerPosition().x);
+  } else {
+    // do nothing on normal click
+  }
+});
+
+add_marker(10, layer);
+
+background.setZIndex(0);
+line.setZIndex(1);
 stage.add(layer);
+
+function add_marker(x_pos) {
+  let clone = marker.clone({
+    x: x_pos,
+    y: stage.getHeight() / 2,
+  });
+  layer.add(clone);
+  layer.draw();
+}
