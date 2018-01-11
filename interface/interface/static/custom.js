b982d8e42118beb70868cc6d690f2cf4a1779b95
Peter Mitrano <mitranopeter@gmail.com>
2018-01-11 17:29:03 -0500

    add go back 1s button
     - remove old code for scrubbing, showing duration, and not-looping

--- interface/interface/static/custom.js
+++ interface/interface/static/custom.js
@@ -16,9 +16,16 @@ window.onload = function() {
   sample_title.innerHTML = samples[sample_idx]['title'];
   audio.load();
   audio.loop = true;
-  $('#loop_checkbox').prop("checked", true);
   $('#interface').css('background', background_color);
+
+  make_interface();
 };
+
+/////////////////////////////////////////////////////////
+// Keyboard Shortcuts
+/////////////////////////////////////////////////////////
+Mousetrap.bind('b', scrub_back);
+Mousetrap.bind('p', play_pause);
 
 /////////////////////////////////////////////////////////
 // Audio Player
@@ -37,35 +44,23 @@ setInterval(function() {
   // set scrubber position
   if (audio.duration > 0) {
     let x = current_time_to_x();
-    scrubber.x(x);
-    layer.draw();
-    set_time();
+    Interface.scrubber.x(x);
+    Interface.layer.draw();
   }
-}, 30);
+}, 25);
 
 function current_time_to_x() {
-  return line_begin + audio.currentTime / audio.duration * (line_end - line_begin);
+  return Interface.line_begin + audio.currentTime / audio.duration * (Interface.line_end - Interface.line_begin);
 }
 
 function x_to_time(x) {
-  return (x - line_begin) / (line_end - line_begin) * audio.duration;
+  return (x - Interface.line_begin) / (Interface.line_end - Interface.line_begin) * audio.duration;
 }
 
 function timeFmt(t) {
   return (t / 60).toFixed(0).padStart(2, '0') + ':' +
       t.toFixed(0).padStart(2, '0');
 }
-
-function set_time() {
-  let now = timeFmt(audio.currentTime);
-  let dur = timeFmt(audio.duration);
-  let duration_string = now + ' / ' + dur;
-  $('#duration').prop('innerHTML', duration_string);
-}
-
-audio.addEventListener('durationchange', function() {
-  set_time();
-});
 
 audio.addEventListener('play', function() {
   let icon = $('#pause_play_button > span');
@@ -85,8 +80,8 @@ audio.addEventListener('pause', function() {
   icon.removeClass('glyphicon-pause');
 });
 
-function set_loop() {
-  audio.loop = $('#loop_checkbox').is(':checked');
+function scrub_back() {
+    audio.currentTime -= 1;
 }
 
 function next_submit() {
@@ -95,17 +90,17 @@ function next_submit() {
 
   // save the current responses
   responses[sample_idx] = [];
-  for (let i = 0; i < markers.length; i++) {
-    responses[sample_idx].push(x_to_time(markers[i].x()));
+  for (let i = 0; i < Interface.markers.length; i++) {
+    responses[sample_idx].push(x_to_time(Interface.markers[i].x()));
   }
 
   // clear current responses
-  let i = markers.length - 1;
+  let i = Interface.markers.length - 1;
   for (; i >= 0; i--) {
-    markers[i].destroy();
-    markers.splice(i, 1);
+    Interface.markers[i].destroy();
+    Interface.markers.splice(i, 1);
   }
-  layer.draw();
+  Interface.layer.draw();
 
   if (sample_idx === samples.length - 1) {
     $('#next-submit-button').prop('disabled', true);
@@ -144,135 +139,123 @@ function next_submit() {
 /////////////////////////////////////////////////////////
 // Konva Canvas
 /////////////////////////////////////////////////////////
+let Interface = {};
 
-let width = 700;
-let height = 50;
+function make_interface() {
+    let width =  $('#interface').width() - 100;
+    let height = 50;
 
-let stage = new Konva.Stage({
-  container: 'canvas',
-  width: width,
-  height: height,
-  fill: '#ff0',
-});
+    Interface.stage = new Konva.Stage({
+      container: 'canvas',
+      width: width,
+      height: height,
+      fill: '#ff0',
+    });
 
-let layer = new Konva.Layer();
-let radius = 10;
-let stroke = 2;
-let line_height = 2;
-let line_begin = 10;
-let line_end = width - 10;
+    Interface.layer = new Konva.Layer();
+    let radius = 9;
+    let stroke = 2;
+    Interface.line_height = 2;
+    Interface.line_begin = 10;
+    Interface.line_end = width - 10;
 
-let line = new Konva.Rect({
-  x: line_begin,
-  y: stage.getHeight() / 2 - line_height / 2,
-  width: line_end - line_begin,
-  height: line_height,
-  fill: '#666',
-});
-layer.add(line);
+    let line = new Konva.Rect({
+      x: Interface.line_begin,
+      y: Interface.stage.getHeight() / 2 - Interface.line_height / 2,
+      width: Interface.line_end - Interface.line_begin,
+      height: Interface.line_height,
+      fill: '#666',
+    });
+    Interface.layer.add(line);
 
-let background = new Konva.Rect({
-  x: 0,
-  y: 0,
-  width: stage.getWidth(),
-  height: stage.getHeight(),
-  fill: background_color,
-});
-layer.add(background);
+    let background = new Konva.Rect({
+      x: 0,
+      y: 0,
+      width: Interface.stage.getWidth(),
+      height: Interface.stage.getHeight(),
+      fill: background_color,
+    });
+    Interface.layer.add(background);
 
-let scrubber_radius = 8;
-let scrubber = new Konva.Circle({
-  x: line_begin,
-  y: stage.getHeight() / 2,
-  radius: scrubber_radius,
-  fill: '#222',
-  draggable: true,
-  dragBoundFunc: function(pos) {
-    audio.currentTime = x_to_time(pos.x);
-    return {
-      x: bound(pos.x),
-      y: this.getAbsolutePosition().y,
-    };
-  },
-});
+    let scrubber_radius = 8;
+    Interface.scrubber = new Konva.Rect({
+      x: Interface.line_begin,
+      y: Interface.stage.getHeight() / 2 - 15,
+      width: 3,
+      height: 30,
+      fill: '#222',
+    });
 
-scrubber.on('mouseover', function() {
-  document.body.style.cursor = 'pointer';
-});
+    Interface.layer.add(Interface.scrubber);
 
-scrubber.on('mouseout', function() {
-  document.body.style.cursor = 'default';
-});
+    Interface.marker = new Konva.Circle({
+      x: Interface.stage.getWidth() / 2,
+      y: Interface.stage.getHeight() / 2,
+      radius: radius,
+      fill: '#4285F422',
+      stroke: '#4285F4',
+      strokeWidth: stroke,
+      draggable: true,
+      dragBoundFunc: function(pos) {
+        return {
+          x: bound(pos.x),
+          y: this.getAbsolutePosition().y,
+        };
+      },
+    });
 
-layer.add(scrubber);
+    Interface.marker.on('mouseover', function() {
+      document.body.style.cursor = 'pointer';
+    });
 
-let marker = new Konva.Circle({
-  x: stage.getWidth() / 2,
-  y: stage.getHeight() / 2,
-  radius: radius,
-  fill: '#4285F422',
-  stroke: '#4285F4',
-  strokeWidth: stroke,
-  draggable: true,
-  dragBoundFunc: function(pos) {
-    return {
-      x: bound(pos.x),
-      y: this.getAbsolutePosition().y,
-    };
-  },
-});
+    Interface.marker.on('mouseout', function() {
+      document.body.style.cursor = 'default';
+    });
 
-marker.on('mouseover', function() {
-  document.body.style.cursor = 'pointer';
-});
+    Interface.marker.on('click', function(event) {
+      if (event.evt.shiftKey) {
+        this.destroy();
+        Interface.layer.draw();
+        let idx = Interface.markers.indexOf(this);
+        if (idx > -1) {
+          Interface.markers.splice(idx, 1);
+        }
+      }
+    });
 
-marker.on('mouseout', function() {
-  document.body.style.cursor = 'default';
-});
+    Interface.layer.on('click', function(event) {
+      if (event.evt.ctrlKey) {
+        // insert new marker
+        let x = stage.getPointerPosition().x;
+        if ((x > line_begin) && (x < line_end)) {
+          add_marker(x);
+        }
+      } else {
+        // do nothing on normal click
+      }
+    });
 
-marker.on('click', function(event) {
-  if (event.evt.shiftKey) {
-    this.destroy();
-    layer.draw();
-    let idx = markers.indexOf(this);
-    if (idx > -1) {
-      markers.splice(idx, 1);
-    }
-  }
-});
-
-layer.on('click', function(event) {
-  if (event.evt.ctrlKey) {
-    // insert new marker
-    let x = stage.getPointerPosition().x;
-    if ((x > line_begin) && (x < line_end)) {
-      add_marker(x);
-    }
-  } else {
-    // do nothing on normal click
-  }
-});
-
-background.setZIndex(0);
-line.setZIndex(1);
-let markers = [];
-init_interface();
-stage.add(layer);
+    background.setZIndex(0);
+    line.setZIndex(1);
+    Interface.markers = [];
+    init_interface();
+    Interface.stage.add(Interface.layer);
+}
 
 function init_interface() {
-  add_marker((line_end + line_begin) / 2);
+  add_marker((Interface.line_end + Interface.line_begin) / 2);
 }
 
 function bound(x) {
-  return Math.max(line_begin, Math.min(line_end, x));
+  return Math.max(Interface.line_begin, Math.min(Interface.line_end, x));
 }
 
 function add_marker(x_pos) {
-  let clone = marker.clone({
+  let clone = Interface.marker.clone({
     x: x_pos,
-    y: stage.getHeight() / 2,
+    y: Interface.stage.getHeight() / 2,
   });
-  layer.add(clone);
-  layer.draw();
-  markers.push(clone);
+  Interface.layer.add(clone);
+  Interface.layer.draw();
+  Interface.markers.push(clone);
 }
