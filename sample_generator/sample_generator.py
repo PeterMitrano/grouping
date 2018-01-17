b2347da21d2a940e15d1d280fd2184fdd3bc5a01
Peter Mitrano <mitranopeter@gmail.com>
2018-01-17 16:47:46 -0500

    add fadeing/noise options

--- sample_generator/sample_generator.py
+++ sample_generator/sample_generator.py
@@ -1,23 +1,37 @@
-from pydub import AudioSegment, generators
+from pydub import AudioSegment, generators, effects
 import sys
-import os
 import argparse
 import numpy as np
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", help='input file')
     parser.add_argument("outfile", help='output file')
-    args=parser.parse_args()
+    parser.add_argument("--noise", action='store_true', help='add noise')
+    args = parser.parse_args()
 
-    SAMPLE_LENGTH = 7000 # milliseconds
+    sample_length = 8000  # milliseconds
+    noise_length = 1000
+    fade_length = 250
+    quiet = -45
 
     song = AudioSegment.from_mp3(args.infile)
-    start_time = np.random.uniform(0, song.duration_seconds*500)
-    sample = song[start_time:start_time + SAMPLE_LENGTH]
-    noise = generators.WhiteNoise().to_audio_segment(duration=500, volume=-30)
-    clip = noise + sample
+    start_time = np.random.uniform(0, song.duration_seconds * 500)
+    clip = song[start_time:start_time + sample_length]
+    noise = generators.WhiteNoise().to_audio_segment(duration=noise_length, volume=quiet)
+
+    # enveloping and fading
+    if args.noise:
+        clip = noise.append(clip, crossfade=noise_length / 2)
+        clip = clip.append(noise, crossfade=noise_length / 2)
+    clip = clip.fade_in(duration=fade_length)
+    clip = clip.fade_out(duration=fade_length)
+
+    # export file
+    # clip = noise + clip + noise
     clip.export(args.outfile)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     sys.exit(main())
