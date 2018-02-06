31a93799249c51e9d14f6efbe401609a28cc8656
Peter Mitrano <mitranopeter@gmail.com>
2018-02-05 22:29:09 -0500

    now records duration of each sample for each person

--- interface/interface/static/custom.js
+++ interface/interface/static/custom.js
@@ -7,19 +7,21 @@ let background_color = '#D9EDF7';
 let start_time = 0;
 let marker_id_counter = 0;
 let trial_id = generateID();
+let trial_start_time;
 
 function Action(type, id) {
   return {
     'type': type,
-    'id': id
+    'id': id,
   };
 }
 
 function Response() {
   return {
     'final_response': [],
-    'edit_history': []
-  }
+    'edit_history': [],
+    'duration_seconds': -1,
+  };
 }
 
 /////////////////////////////////////////////////////////
@@ -38,12 +40,15 @@ window.onload = function() {
 
   make_interface();
 
-  $("#interface").hide();
+  $('#interface').hide();
 };
 
 function show_interface() {
-  $("#confirm").hide();
-  $("#interface").show();
+  $('#confirm').hide();
+  $('#interface').show();
+
+  // also start timer for first trial here
+  trial_start_time = new Date();
 }
 
 /////////////////////////////////////////////////////////
@@ -75,7 +80,8 @@ setInterval(function() {
 }, 25);
 
 function current_time_to_x() {
-  return Interface.line_begin + audio.currentTime / audio.duration * (Interface.line_end - Interface.line_begin);
+  return Interface.line_begin + audio.currentTime / audio.duration *
+      (Interface.line_end - Interface.line_begin);
 }
 
 function x_to_time(x) {
@@ -106,12 +112,12 @@ audio.addEventListener('pause', function() {
 });
 
 function scrub_back() {
-    audio.currentTime -= 1;
+  audio.currentTime -= 1;
 }
 
 function next_submit() {
   // pause the music
-  audio.pause()
+  audio.pause();
 
   // save the current responses for this sample
   for (let i = 0; i < Interface.markers.length; i++) {
@@ -126,6 +132,11 @@ function next_submit() {
   }
   Interface.layer.draw();
 
+  // save how long it took to complete this sample
+  let now = new Date();
+  responses[sample_idx]['duration_seconds'] = now.getTime() - trial_start_time.getTime();
+  trial_start_time = now;
+
   if (sample_idx === samples.length - 1) {
     $('#next-submit-button').prop('disabled', true);
 
@@ -133,23 +144,23 @@ function next_submit() {
     let request = new XMLHttpRequest();
     let finish_time = new Date().getTime();
     let metadata = {
-      // 'trial time': (finish_time - start_time) / 1000.0,
-      'tiral-id': trial_id
+      'tiral-id': trial_id,
     };
     let post_data = {
       'metadata': metadata,
       'samples': samples,
       'responses': responses,
     };
-    let url = "/responses";
+    let url = '/responses';
     request.open('POST', url, true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     request.send(JSON.stringify(post_data));
 
     // FIXME: redirect to debriefing page
     // window.location.href = 'thankyou.html?trial-id=' + trial_id;
   }
   else {
+
     // load the next sample
     sample_idx += 1;
     audio_src.src = samples[sample_idx]['url'];
@@ -174,136 +185,136 @@ function next_submit() {
 let Interface = {};
 
 function make_interface() {
-    let width =  $('#interface').width() - 100;
-    let height = 50;
+  let width = $('#interface').width() - 100;
+  let height = 50;
 
-    Interface.stage = new Konva.Stage({
-      container: 'canvas',
-      width: width,
-      height: height,
-      fill: '#ff0',
-    });
+  Interface.stage = new Konva.Stage({
+    container: 'canvas',
+    width: width,
+    height: height,
+    fill: '#ff0',
+  });
 
-    Interface.layer = new Konva.Layer();
-    let radius = 9;
-    let stroke = 2;
-    Interface.marker_small = 6;
-    Interface.marker_large = 9;
-    Interface.line_height = 2;
-    Interface.line_begin = 10;
-    Interface.line_end = width - 10;
+  Interface.layer = new Konva.Layer();
+  let radius = 9;
+  let stroke = 2;
+  Interface.marker_small = 6;
+  Interface.marker_large = 9;
+  Interface.line_height = 2;
+  Interface.line_begin = 10;
+  Interface.line_end = width - 10;
 
-    let line = new Konva.Rect({
-      x: Interface.line_begin,
-      y: Interface.stage.getHeight() / 2 - Interface.line_height / 2,
-      width: Interface.line_end - Interface.line_begin,
-      height: Interface.line_height,
-      fill: '#666',
-    });
-    Interface.layer.add(line);
+  let line = new Konva.Rect({
+    x: Interface.line_begin,
+    y: Interface.stage.getHeight() / 2 - Interface.line_height / 2,
+    width: Interface.line_end - Interface.line_begin,
+    height: Interface.line_height,
+    fill: '#666',
+  });
+  Interface.layer.add(line);
 
-    let background = new Konva.Rect({
-      x: 0,
-      y: 0,
-      width: Interface.stage.getWidth(),
-      height: Interface.stage.getHeight(),
-      fill: background_color,
-    });
-    Interface.layer.add(background);
+  let background = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: Interface.stage.getWidth(),
+    height: Interface.stage.getHeight(),
+    fill: background_color,
+  });
+  Interface.layer.add(background);
 
-    let scrubber_radius = 8;
-    Interface.scrubber = new Konva.Rect({
-      x: Interface.line_begin,
-      y: Interface.stage.getHeight() / 2 - 15,
-      width: 3,
-      height: 30,
-      fill: '#222',
-    });
+  let scrubber_radius = 8;
+  Interface.scrubber = new Konva.Rect({
+    x: Interface.line_begin,
+    y: Interface.stage.getHeight() / 2 - 15,
+    width: 3,
+    height: 30,
+    fill: '#222',
+  });
 
-    Interface.layer.add(Interface.scrubber);
+  Interface.layer.add(Interface.scrubber);
 
-    Interface.marker = new Konva.Circle({
-      x: Interface.stage.getWidth() / 2,
-      y: Interface.stage.getHeight() / 2,
-      radius: radius,
-      fill: '#4285F422',
-      stroke: '#4285F4',
-      strokeWidth: stroke,
-      draggable: true,
-      dragBoundFunc: function(pos) {
-        // called every time the object is dragged
-        this.was_dragging = true;
-        return {
-          x: bound(pos.x),
-          y: this.getAbsolutePosition().y,
-        };
-      },
-    });
+  Interface.marker = new Konva.Circle({
+    x: Interface.stage.getWidth() / 2,
+    y: Interface.stage.getHeight() / 2,
+    radius: radius,
+    fill: '#4285F422',
+    stroke: '#4285F4',
+    strokeWidth: stroke,
+    draggable: true,
+    dragBoundFunc: function(pos) {
+      // called every time the object is dragged
+      this.was_dragging = true;
+      return {
+        x: bound(pos.x),
+        y: this.getAbsolutePosition().y,
+      };
+    },
+  });
 
-    Interface.marker.marker_id = marker_id_counter++;
+  Interface.marker.marker_id = marker_id_counter++;
 
-    Interface.marker.on('mousedown', function() {
-    });
+  Interface.marker.on('mousedown', function() {
+  });
 
-    Interface.marker.on('mouseup', function() {
-        // end of drag
-        if (this.was_dragging) {
-          responses[sample_idx]['edit_history'].push(Action("drag", this.marker_id));
-        }
-        this.was_dragging = false;
-    });
+  Interface.marker.on('mouseup', function() {
+    // end of drag
+    if (this.was_dragging) {
+      responses[sample_idx]['edit_history'].push(Action('drag', this.marker_id));
+    }
+    this.was_dragging = false;
+  });
 
-    Interface.marker.on('mouseover', function() {
-      document.body.style.cursor = 'pointer';
-    });
+  Interface.marker.on('mouseover', function() {
+    document.body.style.cursor = 'pointer';
+  });
 
-    Interface.marker.on('mouseout', function() {
-      document.body.style.cursor = 'default';
-    });
+  Interface.marker.on('mouseout', function() {
+    document.body.style.cursor = 'default';
+  });
 
-    Interface.marker.on('click', function(event) {
-      if (event.evt.shiftKey) {
-        // delete the marker
-        responses[sample_idx]['edit_history'].push(Action("delete", this.marker_id));
-        this.destroy();
+  Interface.marker.on('click', function(event) {
+    if (event.evt.shiftKey) {
+      // delete the marker
+      responses[sample_idx]['edit_history'].push(Action('delete', this.marker_id));
+      this.destroy();
+      Interface.layer.draw();
+      let idx = Interface.markers.indexOf(this);
+      if (idx > -1) {
+        Interface.markers.splice(idx, 1);
+      }
+    }
+    else if (event.evt.altKey) {
+      // resize the marker
+      responses[sample_idx]['edit_history'].push(Action('resize', this.marker_id));
+      if (this.radius() === Interface.marker_small) {
+        this.setRadius(Interface.marker_large);
         Interface.layer.draw();
-        let idx = Interface.markers.indexOf(this);
-        if (idx > -1) {
-          Interface.markers.splice(idx, 1);
-        }
       }
-      else if (event.evt.altKey) {
-        // resize the marker
-        responses[sample_idx]['edit_history'].push(Action("resize", this.marker_id));
-        if (this.radius() === Interface.marker_small) {
-          this.setRadius(Interface.marker_large);
-          Interface.layer.draw();
-        }
-        else {
-          this.setRadius(Interface.marker_small);
-          Interface.layer.draw();
-        }
+      else {
+        this.setRadius(Interface.marker_small);
+        Interface.layer.draw();
       }
-    });
+    }
+  });
 
-    Interface.layer.on('click', function(event) {
-      if (event.evt.metaKey) {
-        // insert new marker
-        let x = Interface.stage.getPointerPosition().x;
-        if ((x > Interface.line_begin) && (x < Interface.line_end)) {
-          let new_marker = add_marker(x);
-          responses[sample_idx]['edit_history'].push(Action("add", new_marker.marker_id));
-        }
-      } else {
-        // do nothing on normal click
+  Interface.layer.on('click', function(event) {
+    if (event.evt.metaKey) {
+      // insert new marker
+      let x = Interface.stage.getPointerPosition().x;
+      if ((x > Interface.line_begin) && (x < Interface.line_end)) {
+        let new_marker = add_marker(x);
+        responses[sample_idx]['edit_history'].push(Action('add', new_marker.marker_id));
       }
-    });
+    } else {
+      // do nothing on normal click
+    }
+  });
 
-    background.setZIndex(0);
-    line.setZIndex(1);
-    Interface.markers = [];
-    init_interface();
-    Interface.stage.add(Interface.layer);
+  background.setZIndex(0);
+  line.setZIndex(1);
+  Interface.markers = [];
+  init_interface();
+  Interface.stage.add(Interface.layer);
 }
 
 function init_interface() {
@@ -329,8 +340,8 @@ function add_marker(x_pos) {
 
 function generateID() {
   let id = getRandomHex();
-  for (let i =0; i < 8; i++) {
-    id += "::";
+  for (let i = 0; i < 8; i++) {
+    id += '::';
     id += getRandomHex();
   }
   return id;
@@ -339,7 +350,7 @@ function generateID() {
 function getRandomHex() {
   hex = Math.floor(Math.random() * (Math.pow(2, 8) + 1)).toString(16);
   if (hex.length < 2) {
-    hex = "0" + hex;
+    hex = '0' + hex;
   }
   return hex;
 }
