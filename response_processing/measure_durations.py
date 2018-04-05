70a1df3624593b9f6f476228f67743ca52f0cd9a
Peter Mitrano <mitranopeter@gmail.com>
2018-04-05 11:01:59 -0400

    script for measuring how long it is taking people

---
+++ response_processing/measure_durations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser("prints a tree of times for each experiment & trial")
+    parser.add_argument("dumpfile", help="The output of \"flask dumpdb --outfile=dump.json\"")
+    parser.add_argument("-v", help="print each indvidual trial time", action="store_true")
+
+    args = parser.parse_args()
+
+    trials = json.load(open(args.dumpfile, "r"))
+
+    # group by experiment into a dict
+    experiments = {}
+    for trial in trials:
+        if trial["experiment_id"] not in experiments:
+            experiments[trial["experiment_id"]] = []
+        experiments[trial["experiment_id"]].append(trial)
+
+    # print the information
+    for experiment_id, trials_by_experiment in experiments.items():
+        total_time_s = 0
+        for trial in trials_by_experiment:
+            trial_time_s = float(trial["data"]["duration_seconds"])
+            total_time_s += trial_time_s
+        total_time_formatted = str(datetime.timedelta(seconds=total_time_s))
+        stamp = trial["stamp"]
+        print("{:s} ({:s}, {:s})".format(total_time_formatted, experiment_id, stamp))
+        for trial in trials_by_experiment:
+            trial_time_s = float(trial["data"]["duration_seconds"])
+            trial_time_formatted = str(datetime.timedelta(seconds=trial_time_s))
+            db_id = trial["id"]
+            print("  {:s} ({:d})".format(trial_time_formatted, db_id))
+
+
+
+if __name__ == "__main__":
+    main()
